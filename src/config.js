aef2ef30dfa828a1daaf3a4c5907e86d527ad93c
Akshay Chacko <akshaychacko999@gmail.com>
2020-06-05 19:53:22 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     'Akshay Chacko is a software engineer based in San Jose, CA specializing in building and designing applications, websites, and sounds.',
   siteKeywords:
     'Akshay Chacko, Akshay, Chacko, akshaychacko, software engineer, front-end engineer, web developer, javascript, umich',
-  siteUrl: 'https://akshaychacko.tk',
+  siteUrl: 'https://brittanychiang.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
